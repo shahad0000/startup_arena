fae51455c4913d8d9ad6325459d69e9462e07629
SaadAlzarea <sfalzarea@gmail.com>
2025-06-29 14:07:13 +0300

    create router

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,15 +1,43 @@
-import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import Layout from "../components/Layout";
 import Home from "../pages/Home";
+
+import React from "react";
+import { Outlet } from "react-router";
+import LandingPage from "../pages/LandingPage";
+
+const Layout = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [
-      { path: "/", element: <Home /> },
-    ],
+    children: [{ path: "/", element: <LandingPage /> }],
+  },
+  {
+    path: "/admin",
+    element: <Layout />,
+    children: [{ path: "/", element: <LandingPage /> }],
+  },
+  {
+    path: "/investor",
+    element: <Layout />,
+    children: [{ path: "/", element: <LandingPage /> }],
+  },
+  {
+    path: "/founder",
+    element: <Layout />,
+    children: [{ path: "/", element: <LandingPage /> }],
+  },
+  {
+    path: "/critic",
+    element: <Layout />,
+    children: [{ path: "/", element: <LandingPage  /> }],
   },
 ]);
 
