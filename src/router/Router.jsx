087de3a16f8c040586bb446b668e9fd2a6df9939
shouq <shouqbis@gmail.com>
2025-07-03 11:30:26 +0300

    fix responsive

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,42 +1,39 @@
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
-import LandingPage from "../pages/LandingPage";
-import SignUp from "../Component/SignUpFirstStep";
-import SignIn from "../pages/SignIn";
-import SignUpSecondStep from "../Component/SignUpSecondStep";
-import SubmitIdeaForm from "../pages/SubmitIdeaForm";
-import MyIdeas from "../pages/MyIdeas";
-import AllIdeas from "../pages/ExploreStartupIdeas";
-import FeaturedIdeas from "../pages/FeaturedIdeas";
-import IdeaAnalysis from "../pages/IdeaAnalysis";
-import IdeaDetails from "../pages/IdeaDetails";
-import WatchMeetings from "../pages/WatchMeetings";
-import React from "react";
-import ScheduleMeetingForm from "../pages/ScheduleMeetingForm";
-import SignUpForm from "../pages/SignUp";
-import AdminLogin from "../admin/pages/AdminLogin";
-import AdminDashboard from "../admin/pages/AdminDashboard";
-import ManageComments from "../admin/pages/ManageComments";
-import WatchMeetings from "../pages/WatchMeetings ";
-import Nav from "../user/components/Nav";
-import UserProfile from "../Component/UserProfile";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router"
+import LandingPage from "../pages/LandingPage"
+import SignUp from "../Component/SignUpFirstStep"
+import SignIn from "../pages/SignIn"
+import SignUpSecondStep from "../Component/SignUpSecondStep"
+import SubmitIdeaForm from "../pages/SubmitIdeaForm"
+import MyIdeas from "../pages/MyIdeas"
+import AllIdeas from "../pages/ExploreStartupIdeas"
+import FeaturedIdeas from "../pages/FeaturedIdeas"
+import IdeaAnalysis from "../pages/IdeaAnalysis"
+import IdeaDetails from "../pages/IdeaDetails"
+import ScheduleMeetingForm from "../pages/ScheduleMeetingForm"
+import SignUpForm from "../pages/SignUp"
+import AdminLogin from "../admin/pages/AdminLogin"
+import AdminDashboard from "../admin/pages/AdminDashboard"
+import ManageComments from "../admin/pages/ManageComments"
+import WatchMeetings from "../pages/WatchMeetings"
+import Nav from "../user/components/Nav"
 
 const Layout = () => {
   return (
     <>
-    <Nav/>
+      <Nav />
       <Outlet />
     </>
-  );
-};
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      { path: "/", element: <LandingPage /> },
+     /*  { path: "/", element: <LandingPage /> },
       { path: "/signup", element: <SignUpForm /> },
-      { path: "/signin", element: <SignIn /> },
+      { path: "/signin", element: <SignIn /> }, */
       { path: "/submitIdea", element: <SubmitIdeaForm /> },
       { path: "/MyIdeas", element: <MyIdeas /> },
       { path: "/AllIdeas", element: <AllIdeas /> },
@@ -45,7 +42,6 @@ const router = createBrowserRouter([
       { path: "/detailIdea/:id", element: <IdeaDetails /> },
       { path: "/watchMeeting", element: <WatchMeetings /> },
       //{ path: "/profile", element: <UserProfile/> },
-
     ],
   },
   {
@@ -55,6 +51,14 @@ const router = createBrowserRouter([
       { index: true, element: <AdminLogin /> },
       { path: "adminDashboard", element: <AdminDashboard /> },
       { path: "manageComments", element: <ManageComments /> },
+    ],
+  },
+  {
+    path: "/",
+    children: [
+      { index: true, path: "/", element: <LandingPage /> },
+      { path: "/signup", element: <SignUpForm /> },
+      { path: "/signin", element: <SignIn /> },
     ],
   },
   {
@@ -78,10 +82,10 @@ const router = createBrowserRouter([
       // { path: "/", element: <LandingPage /> }
     ],
   },
-]);
+])
 
 const Router = () => {
-  return <RouterProvider router={router} />;
-};
+  return <RouterProvider router={router} />
+}
 
-export default Router;
+export default Router
