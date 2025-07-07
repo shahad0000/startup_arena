aa32aae6379c9252249defc6c22c7dffc579b85e
SaadAlzarea <sfalzarea@gmail.com>
2025-07-07 07:41:59 +0300

    update wacthing loading

--- src/pages/WatchMeetings.jsx
+++ src/pages/WatchMeetings.jsx
@@ -29,7 +29,7 @@ const WatchMeetings = () => {
 
   if (loading) {
     return (
-      <div class="flex justify-center items-center gap-2 min-h-screen min-w-screen">
+      <div class="flex justify-center items-center gap-2 min-h-screen min-w-screen bg-radial-[at_50%_75%] from-sky-200 via-blue-100 to-white to-90%">
         <div class="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
         <div class="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.3s]"></div>
         <div class="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
